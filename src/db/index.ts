a2785034c3d3532e91f9729d3405df0d5d1b55c2
Rasel Hossain Jibon <raselhossainjibon@Rasels-MacBook-Air.local>
2026-06-08 21:26:09 +0600

    feat(db): align user and issue tables with assignment requirements

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,41 +2,37 @@ import { Pool } from "pg";
 import config from "../config/env";
 
 export const pool = new Pool({
-    connectionString :config.Database_URL
+  connectionString: config.Database_URL,
+});
 
-})
-
-
-export const initDb = async()=>{
-    try {
-        // table 1
-        await pool.query(`
+export const initDb = async () => {
+  try {
+    // table 1
+    await pool.query(`
             CREATE TABLE IF NOT EXISTS users(
             ID SERIAL PRIMARY KEY,
-            name VARCHAR(100),
+            name VARCHAR(150),
             email TEXT UNIQUE,
             password TEXT,
-            role VARCHAR(100) DEFAULT 'users',
+            role VARCHAR(150) DEFAULT 'contributor',
             created_at TIMESTAMP DEFAULT NOW (),
-            updated_at TIMESTAMP DEFAULT NOW())`)
-         
-            console.log('database connected successfully');
-            
-            // table 2
-         await pool.query(`
+            updated_at TIMESTAMP DEFAULT NOW())`);
+
+    console.log("database connected successfully");
+
+    // table 2
+    await pool.query(`
             CREATE TABLE IF NOT EXISTS issues (
             ID SERIAL PRIMARY KEY,
-            users_id INT REFERENCES users(id) ON DELETE CASCADE,
-            title VARCHAR(50),
+            reporter_id INT REFERENCES users(id) ON DELETE CASCADE,
+            title VARCHAR(150),
             description TEXT,
-            type VARCHAR(50),
-            status VARCHAR(50) DEFAULT 'pending',
-            reporter_id INT,
+            type VARCHAR(150),
+            status VARCHAR(150) DEFAULT 'open',
+            
             created_at TIMESTAMP DEFAULT NOW (),
-            updated_at TIMESTAMP DEFAULT NOW())`)
-    } catch (error) {
-        console.log(error);
-        
-    }
-}
-
+            updated_at TIMESTAMP DEFAULT NOW())`);
+  } catch (error) {
+    console.log(error);
+  }
+};
